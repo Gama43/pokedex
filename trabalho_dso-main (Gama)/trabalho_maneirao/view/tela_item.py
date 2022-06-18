876884e06d7602b0625ca5aa544ca0c210f89657
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-06-18 18:03:21 -0300

    Delete trabalho_dso-main (Gama)/trabalho_maneirao directory

--- trabalho_dso-main (Gama)/trabalho_maneirao/view/tela_item.py
+++
@@ -1,9 +0,0 @@
-class TelaItem:
-
-    def tela_item(self):
-        print('-----DADOS NOVO ITEM-----')
-        nome = str(input('NOME do novo item: '))
-        quantidade = int(input('Quantidade: '))
-        raridade = int(input('raridade: '))
-
-        return {"nome": nome, "quantidade": quantidade, "raridade": raridade}
--- trabalho_dso-main (Gama)/trabalho_maneirao/view/tela_login.py
+++
@@ -1,7 +0,0 @@
-class TelaLogin:
-
-    def tela_login(self):
-        print('-----TELA DE LOGIN-----')
-        usuario=str(input('Seu usuário: '))
-        idpokedex=str(input('Idpokedex: '))
-        return {"usuario": usuario, "idpokedex":idpokedex}
