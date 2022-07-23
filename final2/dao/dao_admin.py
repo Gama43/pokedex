6ce58db19989e24f7c70b760c1ca627906fd15e7
felipeabe <105646416+felipeabe@users.noreply.github.com>
2022-07-23 10:52:48 -0300

    Create dao_admin.py

---
+++ final2/dao/dao_admin.py
@@ -0,0 +1,18 @@
+from final2.dao.dao_abstract import DAO
+from final2.entidade.treinador import Treinador
+
+class AdminDAO(DAO):
+    def __init__(self):
+        super().__init__('admin.pkl')
+
+    def add(self, treinador: Treinador):
+        if (isinstance(treinador.nome, str) and (treinador is not None)) \
+                and isinstance(treinador, Treinador):
+            super().adicionar(treinador.nome, Treinador)
+
+    def get(self, chave: str):
+        if isinstance(chave, str):
+            return super().get(chave)
+
+    def remove(self, chave: str):
+        return super().remove(chave)
