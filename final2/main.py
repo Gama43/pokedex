e66ded8134b0b5676a70d9aef8c3b17716b39939
Gabriel Rodrigo da Gama <61113905+Gama43@users.noreply.github.com>
2022-07-24 14:13:26 -0300

    Add files via upload

--- final2/main.py
+++ final2/main.py
@@ -1,4 +1,4 @@
-from final2.controladores.controlador_sistema import ControladorSistema
+from controladores.controlador_sistema import ControladorSistema
 
 if __name__ == "__main__":
-  ControladorSistema().inicializa_sistema()
+    ControladorSistema().inicializa_sistema()
